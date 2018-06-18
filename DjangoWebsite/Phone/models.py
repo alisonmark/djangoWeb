71e5461c1abbc7b261821bafbf81b429acac00fa
alisonmark <quanlv.mr@gmail.com>
2018-06-19 00:36:21 +0700

    Initial commit

---
+++ DjangoWebsite/Phone/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    pubDate = models.DateField()
+    
+    def __str__(self):
+        return self.name
+
+
+class iPhone(models.Model):
+    name = models.CharField(max_length=200)
+    iphone_id = models.CharField(max_length=200)
+    imodel = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def getCategory(self):
+        return self.category
+
+
+class Sony(models.Model):
+    name = models.CharField(max_length=200)
+    sony_id = models.CharField(max_length=200)
+    sony_model = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
